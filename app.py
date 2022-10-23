b90b09f8fd4ca3fc094f920da00fd69a9f2c04ed
Manny <manriquezmanny@gmail.com>
2022-10-23 14:16:34 -0700

    Back to where we started

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-blocks = int(input("How tall?"))
-print(blocks)
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
