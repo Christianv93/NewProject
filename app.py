d2e4032d58f53c22c939c7a303db227f55fc5e69
Manny <manriquezmanny@gmail.com>
2022-10-23 14:27:13 -0700

    Linking with url_for

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import A
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
@@ -5,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/guardian")
+def guardian():
+    return render_template("guardian.html")
